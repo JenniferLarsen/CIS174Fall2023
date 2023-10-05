ab247da2ab3d29b9ade74543ce874d1bcedeebdd
Jennifer Larsen <djlarsen1999@gmail.com>
2023-10-05 00:05:41 -0500

    Adding CIS174_TestCoreApp

---
+++ CIS174_TestCoreApp/Models/Assignment61ViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CIS174_TestCoreApp.Models
+{
+    public class Assignment61ViewModel
+    {
+        public List<Student> Students { get; set; }
+        public int AccessLevel { get; set; }
+    }
+}
---
+++ CIS174_TestCoreApp/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace CIS174_TestCoreApp.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+    }
+}
