5b022dd9dae9dbafa3007650c5420758bf55751c
Jennifer Larsen <djlarsen1999@gmail.com>
2023-10-18 19:33:58 -0500

    Corrected Errors - Olympics is now working as intended.

--- Olympics_Larsen/Controllers/HomeController.cs
+++ Olympics_Larsen/Controllers/HomeController.cs
@@ -19,12 +19,15 @@ namespace Olympics_Larsen.Controllers
 
             model.Games = context.Games.ToList();
             model.Sports = context.Sports.ToList();
+            model.Locations = context.Locations.ToList();
 
             IQueryable<Country> query = context.Countries.Include(l => l.Location).Include(s => s.Sport).Include(g => g.Game).OrderBy(c => c.Game);
             if (model.ActiveGame != "all")
                 query = query.Where(c => c.Game.GameID.ToLower() == model.ActiveGame.ToLower());
             if (model.ActiveSport != "all")
                 query = query.Where(c => c.Sport.SportID.ToLower() == model.ActiveSport.ToLower());
+            if (model.ActiveLocation != "all")
+                query = query.Where(l => l.Location.LocationID.ToLower() == model.ActiveLocation.ToLower());
             model.Countries = query.ToList();
             return View(model);
         }
