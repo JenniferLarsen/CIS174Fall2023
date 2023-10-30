ae652dd6895f427362a39c188d77a62f25b25604
Jennifer Larsen <djlarsen1999@gmail.com>
2023-10-29 19:18:10 -0500

    Adding Chapter 11

--- ToDoList/Models/Ticket.cs
+++ ToDoList/Models/Ticket.cs
@@ -8,15 +8,19 @@ namespace ToDoList.Models
         public int TicketID { get; set; }
 
         [Required (ErrorMessage = "Please enter a Name.")]
+        [StringLength (30, ErrorMessage = "Name must be 30 characters or less.")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a Description.")]
+        [StringLength(30, ErrorMessage = "Description must be 30 characters or less.")]
         public string Description { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Please enter a Sprint Number.")]
+        [Required(ErrorMessage = "Please enter a Sprint Number between 1 and 100.")]
+        [Range(1, 100, ErrorMessage = "Sprint Number must be between 1 and 100.")]
         public string SprintNumber { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Please select a Point Value.")]
+        [Required(ErrorMessage = "Please select a Point Value between 1 and 5.")]
+        [Range(1, 5, ErrorMessage = "Point value must be between 1 and 5")]
         public string PointValue { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please select a Status.")]
